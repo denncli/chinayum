eee019ed7088bdc49a9713471d0674efe63ae606
Dennis Li <dennis@Denniss-MacBook-Air.local>
2024-01-02 22:12:38 -0500

    make failure text red

--- chinayum-react/App.js
+++ chinayum-react/App.js
@@ -4,6 +4,8 @@ import { Dimensions, View, Text, Image, TouchableOpacity, StyleSheet } from 'rea
 const App = () => {
   const [currentDishes, setCurrentDishes] = useState([]);
   const [correctDish, setCorrectDish] = useState(null);
+  const [feedback, setFeedback] = useState({ message: '', isCorrect: '' });
+
 
   // Static dish data with required images
   const dishes = [
@@ -32,16 +34,25 @@ const App = () => {
 
   const handleDishPress = dish => {
     if (dish.name === correctDish.name) {
-      alert('Correct!', 'You have selected the right dish.', [{ text: 'Next', onPress: pickRandomDishes }]);
-      pickRandomDishes();
+      setFeedback({ message: 'Correct! You have selected the right dish.', isCorrect: 'true' });
+      setTimeout(() => {
+        setFeedback({ message: '', isCorrect: 'false' });
+        pickRandomDishes();
+      }, 1000);
     } else {
-      alert('Wrong', 'Try again.');
+      setFeedback({ message: 'Wrong, try again.', isCorrect: 'false' });
+      setTimeout(() => {
+        setFeedback({ message: '', isCorrect: '' });
+      }, 2000);
     }
   };
 
   return (
     <View style={styles.container}>
       {correctDish && <Text style={styles.title}>{correctDish.name}</Text>}
+      <Text style={[styles.feedbackText, feedback.isCorrect === 'false' ? styles.incorrectFeedback : styles.correctFeedback]}>
+        {feedback.message}
+      </Text>
       <View style={styles.dishesContainer}>
         {currentDishes.map((dish, index) => (
           <TouchableOpacity key={index} onPress={() => handleDishPress(dish)}>
@@ -52,6 +63,7 @@ const App = () => {
     </View>
   );
 };
+
 
 // Get the screen width
 const screenWidth = Dimensions.get('window').width;
@@ -78,6 +90,16 @@ const styles = StyleSheet.create({
     margin: 5,
     resizeMode: 'contain', // Ensures the image scales correctly within the given dimensions
   },
+  feedbackText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  correctFeedback: {
+    color: 'green',
+  },
+  incorrectFeedback: {
+    color: 'red',
+  }
 });
 
 export default App;
